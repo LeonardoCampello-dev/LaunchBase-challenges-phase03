c5895c0b9b7d61da17b0f5b706642fda37272ef9
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-07-16 23:05:06 -0300

    Refactoring application and configuring the database

---
+++ src/app/controllers/teachers.js
@@ -0,0 +1,37 @@
+const { age, date } = require('../../lib/utils')
+
+module.exports = {
+    index(req, res) {
+        return res.render("teachers/index")
+    },
+    create(req, res) {
+        return res.render("teachers/create")       
+    },
+    post(req, res) {
+        const keys = Object.keys(req.body)
+
+        for (let key of keys) {
+            if (req.body[key] == "") return res.send("Por favor, preencha todos os campos do formulário!")
+        }
+    
+        let { avatar_url, name, birth, educational_level, class_type, services } = req.body   
+    },
+    show(req, res) {
+        return   
+    },
+    edit(req, res) {
+        const keys = Object.keys(req.body)
+
+        for (let key of keys) {
+            if (req.body[key] == "") return res.send("Por favor, preencha todos os campos do formulário!")
+        }
+    
+        return
+    },
+    update(req, res) {
+        return
+    },
+    delete(req, res) {
+        return
+    }
+}
