19de9010364be0152081863af5f86ee233937d77
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-10-13 19:36:49 -0300

    Create model Base

--- src/app/models/Student.js
+++ src/app/models/Student.js
@@ -1,40 +1,11 @@
 const db = require('../../config/db')
-const { date } = require('../../lib/utils')
+
+const Base = require('./Base')
+
+Base.init({ table: 'students' })
 
 module.exports = {
-    async all() {
-        const results = await db.query(`SELECT * FROM students`)
-
-        return results.rows
-    },
-    async create(data) {
-        const query = `
-        INSERT INTO students (
-            avatar_url,
-            name,
-            email,
-            birth,
-            school_year,
-            workload,
-            teacher_id
-        ) VALUES ($1, $2, $3, $4, $5, $6, $7)
-        RETURNING id
-    `
-
-        const values = [
-            data.avatar_url,
-            data.name,
-            data.email,
-            date(data.birth).iso,
-            data.school_year,
-            data.workload,
-            data.teacher
-        ]
-
-        const results = await db.query(query, values)
-
-        return results.rows[0].id
-    },
+    ...Base,
     async find(id) {
         const query = `
         SELECT students.*, teachers.name AS teacher_name 
@@ -46,35 +17,6 @@ module.exports = {
         const results = await db.query(query, [id])
 
         return results.rows[0]
-    },
-    update(data) {
-        const query = `
-        UPDATE students SET
-            avatar_url = ($1),
-            name = ($2),
-            email = ($3),
-            birth = ($4),
-            school_year = ($5),
-            workload = ($6),
-            teacher_id = ($7)
-        WHERE id = $8
-        `
-
-        const values = [
-            data.avatar_url,
-            data.name,
-            data.email,
-            data.birth,
-            data.school_year,
-            data.workload,
-            data.teacher,
-            data.id
-        ]
-
-        return db.query(query, values)
-    },
-    delete(id) {
-        return db.query(`DELETE FROM students WHERE id = $1`, [id])
     },
     async teachersSelectOptions() {
         const results = await db.query(`SELECT name, id FROM teachers`)
--- src/app/models/Teacher.js
+++ src/app/models/Teacher.js
@@ -1,53 +1,11 @@
 const db = require('../../config/db')
-const { date } = require('../../lib/utils')
+
+const Base = require('./Base')
+
+Base.init({ table: 'teachers' })
 
 module.exports = {
-    async all() {
-        const query = `
-        SELECT teachers.*, count(students) AS total_students
-        FROM teachers
-        LEFT JOIN students ON (students.teacher_id = teachers.id)
-        GROUP BY teachers.id
-        ORDER BY teachers.id
-        `
-
-        const results = await db.query(query)
-
-        return results.rows
-    },
-    async create(data) {
-        const query = `
-        INSERT INTO teachers (
-            avatar_url,
-            name,
-            birth,
-            educational_level,
-            class_type,
-            subjects_taught,
-            created_at
-        ) VALUES ($1, $2, $3, $4, $5, $6, $7)
-        RETURNING id
-    `
-
-        const values = [
-            data.avatar_url,
-            data.name,
-            date(data.birth).iso,
-            data.educational_level,
-            data.class_type,
-            data.subjects_taught,
-            date(Date.now()).iso
-        ]
-
-        const results = await db.query(query, values)
-
-        return results.rows[0].id
-    },
-    async find(id) {
-        const results = await db.query(`SELECT * FROM teachers WHERE id = $1`, [id])
-
-        return results.rows[0]
-    },
+    ...Base,
     async findBy(filter) {
         const query = `
         SELECT teachers.*, count(students) AS total_students
@@ -62,33 +20,6 @@ module.exports = {
         const results = await db.query(query)
 
         return results.rows
-    },
-    update(data) {
-        const query = `
-        UPDATE teachers SET
-            avatar_url = ($1),
-            name = ($2),
-            birth = ($3),
-            educational_level = ($4),
-            class_type = ($5),
-            subjects_taught = ($6)
-        WHERE id = $7
-        `
-
-        const values = [
-            data.avatar_url,
-            data.name,
-            data.birth,
-            data.educational_level,
-            data.class_type,
-            data.subjects_taught,
-            data.id
-        ]
-
-        return db.query(query, values)
-    },
-    delete(id) {
-        return db.query(`DELETE FROM teachers WHERE id = $1`, [id])
     },
     async paginate({ filter, limit, offset }) {
         let query = '',
